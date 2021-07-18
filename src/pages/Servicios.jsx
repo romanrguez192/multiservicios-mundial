0ff08d733348c77ef498f03eb258756affe30da9
Román Rodríguez <rdrodriguez.19@est.ucab.edu.ve>
2021-07-18 18:56:43 -0400

    Preparación servicios
    Acomodando

--- src/pages/Servicios.jsx
+++ src/pages/Servicios.jsx
@@ -1,43 +1,66 @@
-import React from "react";
-import  { 
-  makeStyles, 
-}from '@material-ui/core';
-import Sidebar from '../components/Sidebar';
+import React, { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core";
+import Sidebar from "../components/Sidebar";
 import TableServicios from "../components/TableServicios";
-
 
 // ESTILOS
 const useStyles = makeStyles({
   divFlex: {
-    display: 'inline-flex',
-    marginBottom: '10pt',
+    display: "inline-flex",
+    marginBottom: "10pt",
   },
-  containerServicios:{
-    flexGrow: '1',
-    marginTop: '60pt',
+  containerServicios: {
+    flexGrow: "1",
+    marginTop: "60pt",
   },
   root: {
-    display: 'flex',
+    display: "flex",
   },
   tableContainer: {
-    width: '80vw',
-    margin: 'auto',
+    width: "80vw",
+    margin: "auto",
   },
 });
 
-
 const Servicios = () => {
-    const classes = useStyles();
+  const classes = useStyles();
+  const [servicios, setServicios] = useState([]);
+  const [loadingS, setLoadingS] = useState(true);
 
+  useEffect(() => {
+    const getServicios = async () => {
+      const url = "http://localhost:4000/api/servicios";
 
+      const response = await fetch(url);
+
+      if (!response.ok) {
+        // TODO: Error
+        return console.log("Oh no");
+      }
+
+      const servicios = await response.json();
+
+      setServicios(servicios);
+      setLoadingS(false);
+    };
+
+    getServicios();
+  }, []);
 
   return (
     <>
       <div className={classes.root}>
-        <Sidebar page="servicios"/>
+        <Sidebar page="servicios" />
         <main className={classes.containerServicios}>
           <div className={classes.tableContainer}>
-            <TableServicios />
+            <TableServicios
+              // props
+              {...{
+                servicios,
+                setServicios,
+                loadingS,
+              }}
+            />
           </div>
         </main>
       </div>
