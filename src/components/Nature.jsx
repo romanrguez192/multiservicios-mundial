3bc81f0ac4b72f8f92cbf03ff18e1780133bb173
Jose Saad <josemiguelsaad13@gmail.com>
2021-07-20 17:27:43 -0400

    bug del carrusel arreglado

--- src/components/Nature.jsx
+++ src/components/Nature.jsx
@@ -8,7 +8,6 @@ import Nature3 from '../img/nature3.svg';
 import Nature4 from '../img/nature4.svg';
 import Nature5 from '../img/nature5.svg';
 import Nature6 from '../img/nature6.svg';
-
 // ESTILOS
 const useStyles = makeStyles({
   carrusel: {
@@ -22,6 +21,11 @@ const useStyles = makeStyles({
     bottom: '0',
     color: '#199479'
   },
+  contImage: {
+    height: '300px',
+    width: '309px',
+    margin: 'auto',
+  }
 });
 
 const Nature = (props) => {
@@ -29,44 +33,43 @@ const Nature = (props) => {
   const CarouselUI = ({ children }) => <div className={classes.carrusel}>{children}</div>;
   const Carousel = makeCarousel(CarouselUI);
   
-
   return (
     <Carousel defaultWait={7000}>
       <Fade>
-        <div>
+        <div className={classes.contImage}>
           <img src={Nature3} />
-          <h2>Estamos comprometidos con el medio ambiente y con tu vehículo</h2>
         </div>
+        <h2>Estamos comprometidos con el medio ambiente y con tu vehículo</h2>
       </Fade>
       <Fade>
-        <div>
+        <div className={classes.contImage}>
           <img src={Nature1} />
-          <h2>Trata al ambiente igual de bien que a tu vehículo</h2>
         </div>
+        <h2>Trata al ambiente igual de bien que a tu vehículo</h2>    
       </Fade>
       <Fade>
-        <div>
+        <div className={classes.contImage}>
           <img src={Nature2} />
-          <h2>¿Sabías que los productos ecológicos no utilizan ningún producto químico?</h2>
         </div>
+        <h2>¿Sabías que los productos ecológicos no utilizan ningún producto químico?</h2>
       </Fade>
       <Fade>
-        <div>
+        <div className={classes.contImage}>
           <img src={Nature4} />
-          <h2>La tierra no es una herencia de nuestros padres, sino un préstamo de nuestros hijos</h2>
         </div>
+        <h2>La tierra no es una herencia de nuestros padres, sino un préstamo de nuestros hijos</h2>
       </Fade>
       <Fade>
-        <div>
+        <div className={classes.contImage}>
           <img src={Nature5} />
-          <h2>Si pudiéramos ver el milagro de una sola flor claramente, nuestra vida entera cambiaría</h2>
         </div>
+        <h2>Si pudiéramos ver el milagro de una sola flor claramente, nuestra vida entera cambiaría</h2>
       </Fade>
       <Fade>
-        <div>
+        <div className={classes.contImage}>
           <img src={Nature6} />
-          <h2>Recuerda siempre que la naturaleza nunca ha traicionado a quién la ha amado</h2>
         </div>
+        <h2>Recuerda siempre que la naturaleza nunca ha traicionado a quién la ha amado</h2>
       </Fade>
     </Carousel>
   );
