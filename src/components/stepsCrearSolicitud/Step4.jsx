2884a2b10b9f289ae5a69c477259624bdc386e90
Jose Saad <josemiguelsaad13@gmail.com>
2021-07-29 13:20:59 -0400

    seguire despues de numerico xd

--- src/components/stepsCrearSolicitud/Step4.jsx
+++ src/components/stepsCrearSolicitud/Step4.jsx
@@ -1,7 +1,59 @@
 import React, { useState } from "react";
+import {
+   makeStyles,
+   Paper,
+} from "@material-ui/core";
+import InputDate from "./InputDate";
+import Fade from "react-reveal/Fade";
+import Input from "../inputs/Input";
+
+// Estilos
+const useStyles = makeStyles({
+  container: {
+    width: "80vw",
+    margin: "auto",
+    marginBottom: "20pt",
+  },
+  containerData: {
+    margin: "auto",
+    alignItems: "center",
+    textAlign: "center",
+  },
+  subtitleFH: {
+    fontFamily: 'Quicksand',
+    fontStyle: 'normal',
+    fontSize: '15pt',
+    fontWeight: 'bold',
+    lineHeight: '28px',
+    color: '#199479',
+    paddingTop: '20pt',
+  },
+  subtitleDA: {
+    fontFamily: 'Quicksand',
+    fontStyle: 'normal',
+    fontSize: '15pt',
+    fontWeight: 'bold',
+    lineHeight: '28px',
+    color: '#199479',
+    paddingBottom: '20pt',
+  },
+});
 
 const Step4 = () => {
-  return <h1>A</h1>;
+  const classes = useStyles();
+
+  return (
+    <Fade>
+      <Paper className={classes.container}>
+        <div className={classes.containerData}>
+          <p className={classes.subtitleFH}>Fecha y hora estimada para la salida</p>
+          <InputDate/>
+          <p className={classes.subtitleDA}>Datos del autorizado para retirar el vehículo</p>
+          
+        </div>
+      </Paper>
+    </Fade>
+  );
 };
 
 export default Step4;
