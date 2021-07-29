96897f2169c384055a9b179fa65d34d44c69e647
Román Rodríguez <rdrodriguez.19@est.ucab.edu.ve>
2021-07-29 11:21:07 -0400

    Seleccionar servicio sin reserva
    Todo bien, al parecer

--- src/components/stepsCrearSolicitud/Step3.jsx
+++ src/components/stepsCrearSolicitud/Step3.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import TableReservacionesCliente from "../tables/TableReservacionesCliente";
-// import TableServiciosSinReserva from "../tables/TableServiciosSinReserva";
+import TableServiciosSinReserva from "../tables/TableServiciosSinReserva";
 import { makeStyles } from "@material-ui/core";
 
 // Estilos
@@ -11,12 +11,13 @@ const useStyles = makeStyles({
   },
 });
 
-const Step3 = ({ servicios, setServicios, reservas, setReservas, cliente }) => {
+const Step3 = ({ setServicios, setReservas, cliente }) => {
   const classes = useStyles();
 
   return (
     <div className={classes.tableContainer}>
       <TableReservacionesCliente setReservas={setReservas} cedCliente={cliente.cedCliente} />
+      <TableServiciosSinReserva setServicios={setServicios} />
     </div>
   );
 };
--- src/pages/CrearSolicitud.jsx
+++ src/pages/CrearSolicitud.jsx
@@ -78,7 +78,7 @@ const CrearSolicitud = () => {
       case 1:
         return <Step2 {...{ vehiculo, setVehiculo, cliente, setReservas, setServicios }} />;
       case 2:
-        return <Step3 {...{ servicios, setServicios, reservas, setReservas, cliente }} />;
+        return <Step3 {...{ setServicios, setReservas, cliente }} />;
       case 3:
         return <Step4 />;
       default:
