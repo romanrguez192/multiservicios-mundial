397e3ddea15fcb06394ce3b571d4693c9aab651b
Jose Saad <josemiguelsaad13@gmail.com>
2021-07-31 19:49:34 -0400

    algunas tablas para estadisticas

---
+++ src/components/tables/TableEstServiciosSolicitados.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Table from "./Table";
+
+//Tabla de Marca de vehículos que más atendemos por tipo de servicio.
+const TableEstserviciosSolicitados = (props) => {
+  const columns = [
+    {
+      title: "Código",
+      field: "codigo",
+    },
+    {
+      title: "Nombre",
+      field: "nombre",
+    },
+    {
+      title: "Nº de veces que se solicitó",
+      field: "numSolicitudes",
+    },
+  ];
+
+  return (
+    <div>
+      <Table
+        title="Servicios solicitados"
+        columns={columns}
+        //data={lineas}
+        //isLoading={loadingL}
+        {...props}
+      />
+    </div>
+  );
+};
+
+export default TableEstserviciosSolicitados;
